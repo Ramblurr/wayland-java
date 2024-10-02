1457b8f0f6120e72ba72528b1b40847833864dc9
Casey Link <unnamedrambler@gmail.com>
2024-10-02 09:37:01 +0200

    start jar/pom publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,64 @@
 import org.gradle.api.tasks.testing.logging.TestLogEvent
+import java.io.ByteArrayOutputStream
+
+val version = "2.0.0"
+val suffix = "SNAPSHOT"
+
+// Strings embedded into the build.
+var gitRevision by extra("")
+var waylandJavaVersion by extra("")
+
+
+val gitDescribe: String? by lazy {
+    val stdout = ByteArrayOutputStream()
+    try {
+        rootProject.exec {
+            commandLine("git", "describe", "--tags")
+            standardOutput = stdout
+        }
+        stdout.toString().trim().replace("-g", "-")
+    } catch (e: Exception) {
+        null
+    }
+}
+
+val gitBranch: String? by lazy {
+    val stdout = ByteArrayOutputStream()
+    try {
+        rootProject.exec {
+            commandLine("git", "rev-parse", "--abbrev-ref", "HEAD")
+            standardOutput = stdout
+        }
+        stdout.toString().trim()
+    } catch (e: Exception) {
+        null
+    }
+}
+
+if ("release" !in gradle.startParameter.taskNames) {
+    val hash = this.gitDescribe
+
+    if (hash == null) {
+        gitRevision = "dirty"
+        waylandJavaVersion = "$version-dirty"
+        project.logger.lifecycle("Building SNAPSHOT (no .git folder found)")
+    } else {
+        gitRevision = hash
+        waylandJavaVersion = "$hash-SNAPSHOT"
+        project.logger.lifecycle("Building SNAPSHOT ($gitBranch): $gitRevision")
+    }
+} else {
+    gitRevision = ""
+    waylandJavaVersion = if (suffix.isNotEmpty()) "$version-$suffix" else version;
+    project.logger.lifecycle("Building RELEASE ($gitBranch): $waylandJavaVersion")
+}
+
 
 plugins {
     idea
     java
     `java-library`
+    `maven-publish`
 }
 allprojects {
     group = "org.freedesktop"
@@ -23,15 +78,58 @@ subprojects {
             languageVersion.set(JavaLanguageVersion.of(22))
         }
     }
-    val mavenProjects = arrayOf("wayland-client", "wayland-server", "wayland-scanner")
+    val mavenProjects = arrayOf("wayland-client", "wayland-server", "wayland-scanner", "stubs-shared", "jextract", "stubs-client", "stubs-server")
     if (project.name in mavenProjects) {
-//        apply(plugin = "maven-publish")
+        apply(plugin = "maven-publish")
 //        apply(plugin = "signing")
 
         java {
             withJavadocJar()
             withSourcesJar()
         }
+        publishing {
+            publications {
+                create<MavenPublication>(project.name) {
+                    from(components["java"])
+                }
+                register("mavenJava", MavenPublication::class) {
+                    from(components["java"])
+                    groupId = "org.freedesktop"
+                    artifactId = project.name
+                    version = waylandJavaVersion
+
+                    pom {
+                        name = "wayland-java"
+                        description = "Wayland bindings for java/jvm"
+                        url = "https://github.com/ramblurr/wayland-java"
+                        licenses {
+                            license {
+                                name = "The Apache License 2.0"
+                                url = "https://opensource.org/licenses/Apache-2.0"
+                            }
+                        }
+                        developers {
+                            developer {
+                                id = "Zubnix"
+                                name = "Erik De Rijcke"
+                                email = "DeRijcke.Erik@gmail.com"
+                            }
+                            developer {
+                                id = "ramblurr"
+                                name = "Casey Link"
+                                email = "casey@outskirtslabs.com"
+                            }
+                        }
+                        scm {
+                            connection = "scm:git:git://github.com/ramblurr/wayland-java.git"
+                            developerConnection = "scm:git:git@github.com:ramblurr/wayland-java.git"
+                            url = "https://github.com/ramblurr/wayland-java"
+                        }
+                    }
+                }
+            }
+        }
+
     }
 
     tasks.withType<Test> {
